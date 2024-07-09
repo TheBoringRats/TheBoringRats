a9a5a84d579ff517f43e773e615b3fd5ba3176b1
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2024-07-10 01:55:34 +0600

    update

--- src/components/Hader.jsx
+++ src/components/Hader.jsx
@@ -1,35 +1,83 @@
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
-import { HambugerMenu } from "./design/Header"; 
+import { HambugerMenu } from "./design/Header";
+import { useState } from "react";
+import {disablePageScroll, enablePageScroll} from "scroll-lock";
 const Header = () => {
-  const pathname = useLocation()
+  const pathname = useLocation();
+  const [oepnNavigation, setoepnNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    if (oepnNavigation) {
+      setoepnNavigation(false);
+      enablePageScroll();
+    } else {
+      setoepnNavigation(true);
+      disablePageScroll();
+    }
+  };
+  const handleClick = () => {
+    if (!oepnNavigation) return;
+    enablePageScroll();
+    setoepnNavigation(false);
+  };
 
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ">
+    <div
+      className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        oepnNavigation ? "bg-n-8" : "bg-n-8/90 lg:backdrop-blur-sm "
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a classname="block w-[12rem] xl:mr-8" href="">
           <img src={brainwave} width={190} height={40} alt="TheBoaringRats" />
         </a>
-        <nav className="hodden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+        <nav
+           className={`${
+            oepnNavigation ? "flex" : "hidden"
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+        >
+        
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
-              <a key={item.id} href={item.url}
-              className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-4 ${item.onlyMobile ? 'lg:hidden':""}  px-6 py-6 md:py-8 lg:-mr-0.25 lf:text-xs lg:font-semibold ${item.url===pathname.hash ? 'z-2 lg:text-n-1':'lg:text-n-1/50'} lg:leading-5 lg:hover:text-n-1 xl:px-12`}>
+              <a
+                key={item.id}
+                href={item.url}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                  item.onlyMobile ? "lg:hidden" : ""
+                }  px-6 py-6 md:py-8 lg:-mr-0.25 lf:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+              >
                 {item.title}
               </a>
             ))}
           </div>
+          <HambugerMenu />
         </nav>
 
-        <a href="#signup" className="button hidden mr-8 text-n-1/50 transition-colore hover:text-n-1 lg:block">
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colore hover:text-n-1 lg:block"
+        >
           New account
         </a>
-        <Button className="hidden lg:flex "herf="#login">
-          Sign in
 
+        <Button className="hidden lg:flex " herf="#login">
+          Sign in
+        </Button>
+
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
+          <MenuSvg openNavigation={oepnNavigation} />
         </Button>
       </div>
     </div>
